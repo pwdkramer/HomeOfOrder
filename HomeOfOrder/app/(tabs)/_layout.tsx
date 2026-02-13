0d64c6d9c411b22ad799b7567f658a2b83071fe6
patkra <patkra@pdx.edu>
2026-02-13 11:20:05 -0800

    creating my own pages

--- HomeOfOrder/app/(tabs)/_layout.tsx
+++ HomeOfOrder/app/(tabs)/_layout.tsx
@@ -1,35 +1,34 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Home'
+                }}
+            />
+            <Tabs.Screen
+                name="all_tasks"
+                options={{
+                    title: 'All Tasks'
+                }}
+            />
+            <Tabs.Screen
+                name="my_tasks"
+                options={{
+                    title: 'My Tasks'
+                }}
+            />
+            <Tabs.Screen
+                name="meal_plan"
+                options={{
+                    title: 'Meal Plan'
+                }}
+            />
+        </Tabs>
+    );
 }
